1e048004d25c05378928e47b9808e37b913290b1
Nurudeen Abdulsalaam <olaitansalaam2012@gmail.com>
2022-02-03 05:09:10 +0100

    Delete fxtn.py

--- fxtn.py
+++
@@ -1,9 +0,0 @@
-def ola(abi):
-    abi.insert(len(abi)-1, 'and')
-
-spam = ['apples', 'bananas', 'tofu', 'cats', 'lion']
-new_spam = ['apples', 'bananas', 'tofu', 'cats', 'lion', 'yes o', 'okay']
-ola(spam)
-ola(new_spam)
-#print(spam)
-#print(new_spam)
